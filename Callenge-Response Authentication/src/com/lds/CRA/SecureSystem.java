aaa03834ecb7aa1af250d968d8e01d38fefa61a1
Lyndon Darwin-Shelton <45465765+Lyndondshelton@users.noreply.github.com>
2019-09-22 15:46:10 -0400

    Update SecureSystem.java

--- Callenge-Response Authentication/src/com/lds/CRA/SecureSystem.java
+++ Callenge-Response Authentication/src/com/lds/CRA/SecureSystem.java
@@ -1,49 +1,47 @@
 package com.lds.CRA;
 
-import com.lds.CRA.obj.Client;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+
+import com.lds.CRA.obj.Client;
 
 public class SecureSystem {
 
 	private static Client c = new Client("Lyndon", "p@ssW0rd");
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		String sysHash;
 		String userHash;
 
 		System.out.println("Please enter your username.");
 		String usr = c.getUsername();
 		System.out.printf("Username recieved: %s\n", usr);
-		
+
 		int nonce = login(usr);
 
 		System.out.printf("Hashing...\n");
 		sysHash = hash(nonce, c.getPassword());
-		System.out.printf("System hash created: %s\n", sysHash);
-		
+
 		System.out.println("\nPlease enter password");
-		String pass = c.getPassword();
-		System.out.printf("Password recieved: %s\n", pass);
+		String pass = in.readLine();
 		userHash = hash(nonce, pass);
 
 		authenticate(sysHash, userHash);
 	}
 
-	/*
-	 * When a valid user logs on, the system will generate a random number(nonce)
-	 * that will be hashed with the user's known password and the hash will be
-	 * stored in the system.
-	 */
 	public static int login(String username) {
 		String user = c.getUsername();
 		int nonce = 0;
 
-		if (user == username) {
+		if (user.equals(username)) {
 			System.out.println("Username is valid...");
 			nonce = (int) (Math.random() * 1000);
-			System.out.printf("\nGenerating nonce = %d\n", nonce);
+			System.out.printf("\nGenerating nonce...\n");
 
 		} else {
 			System.out.printf("Username is not valid. Please try again...\n");
@@ -51,23 +49,19 @@ public class SecureSystem {
 		return nonce;
 	}
 
-	
 	public static String hash(int r, String password) {
 		String hash = r + password;
+		MessageDigest md;
 		try {
-			MessageDigest ms = MessageDigest.getInstance("SHA-1");
-			
-			byte[] arr = ms.digest(hash.getBytes());
-			
-			BigInteger num = new BigInteger(1, arr);
-			
-			String hex = num.toString(16);
-			
-			while(hex.length() < 32) {
+			md = MessageDigest.getInstance("SHA-1");
+			byte[] arr = md.digest(hash.getBytes());
+			BigInteger bInt = new BigInteger(1, arr);
+			String hex = bInt.toString(16);
+			while (hex.length() < 32) {
 				hex = "0" + hex;
 			}
-			
 			return hex;
+
 		} catch (NoSuchAlgorithmException e) {
 			throw new RuntimeException(e);
 		}
@@ -76,12 +70,8 @@ public class SecureSystem {
 	public static void authenticate(String h1, String h2) {
 		if (h1.equals(h2)) {
 			System.out.printf("\nCongratulations! The passwords match!\n");
-			System.out.printf("sysHash = %s\n", h1);
-			System.out.printf("userHash = %s\n", h2);
 		} else {
 			System.out.printf("\nSorry, an incorrect password was given.\n");
-			System.out.printf("sysHash = %s\n", h1);
-			System.out.printf("userHash = %s\n", h2);
 		}
 	}
 
